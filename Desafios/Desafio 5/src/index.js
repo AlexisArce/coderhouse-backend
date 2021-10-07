0ab4a8eb558dd74636de76f1b23f7f3887dd4c88
aarce <Arce.Alexis@hotmail.com>
2021-10-07 17:01:40 -0300

    Adds product POST endpoit

--- Desafios/Desafio 5/src/index.js
+++ Desafios/Desafio 5/src/index.js
@@ -3,10 +3,14 @@ import productsRoutes from "./routes/productsRoutes";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
-app.use("/productos", productsRoutes);
+app.use("/products", productsRoutes);
 
 app.listen(3004, () => {
   console.log("server is running on port 3004");
--- Desafios/Desafio 5/src/routes/productsRoutes.js
+++ Desafios/Desafio 5/src/routes/productsRoutes.js
@@ -6,12 +6,25 @@ const productsRoutes = new Router();
 
 productsRoutes.get("/", async (req, res) => {
   try {
-    const response = await productsApi.get("/");
+    const response = await productsApi.get("/productos");
     res.render("index", { products: response.data });
   } catch (error) {
     console.log(error);
     res.render("index", { products: [] });
   }
+});
+
+productsRoutes.get("/register", async (req, res) => {
+  res.render("register", {});
+});
+
+productsRoutes.post("/register", async (req, res) => {
+  if (req.body.title) {
+    const data = { ...req.body };
+    const createdProduct = await productsApi.post("/productos", data);
+
+    res.redirect("/");
+  } else res.status(400).send({ error: "debe indicar el nombre del producto" });
 });
 
 export default productsRoutes;
