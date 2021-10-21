5231dbfc7595493540500f278f06a09e1647d9e4
aarce <Arce.Alexis@hotmail.com>
2021-10-21 16:05:35 -0300

    Fixes cart endpoints

--- Proyecto final/src/routes/cart.js
+++ Proyecto final/src/routes/cart.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Container from "../dataAccess/container";
 import * as path from "path";
+import { v4 as uuidv4 } from "uuid";
 
 const { Router } = express;
 const router = new Router();
@@ -15,15 +16,21 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const cart = await container.getById(req.params.id);
-  if (cart) res.send(cart);
+  if (cart) return res.send(cart);
+  else res.status(404).json({ error: "carrito no encontrado" });
+});
+
+router.get("/:id/productos", async (req, res) => {
+  const cart = await container.getById(req.params.id);
+  if (cart) return res.send(cart.products);
   else res.status(404).json({ error: "carrito no encontrado" });
 });
 
 router.post("/", async (req, res) => {
-  const data = { ...req.body };
-  const createdCart = await container.save(data);
+  const cart = { id: uuidv4(), products: [] };
+  await container.save(cart);
 
-  res.status(201).send(createdCart);
+  res.status(201).json({ id: cart.id });
 });
 
 router.delete("/:id", async (req, res) => {
@@ -41,10 +48,16 @@ router.post("/:id/productos", async (req, res) => {
 
   if (!cart) res.status(404).json({ error: "carrito no encontrado" });
 
-  cart.products = req.body.products;
-  await container.updateCart(cart);
+  if (req.body.products && req.body.products.length) {
+    cart.products = [...cart.products, ...req.body.products];
+    await container.updateCart(cart);
 
-  res.status(201);
+    res.status(201).json({ msg: "los productos fueron agregados al carrito" });
+  } else {
+    res
+      .status(400)
+      .json({ error: "Debe enviar una lista de productos válida" });
+  }
 });
 
 router.delete("/:id/productos/:id_prod", async (req, res) => {
@@ -61,7 +74,7 @@ router.delete("/:id/productos/:id_prod", async (req, res) => {
   );
   cart.products = filteredProducts;
 
-  await container.updateCart(data);
+  await container.updateCart(cart);
 
   res.send("producto eliminado del carrito");
 });
