d54341bfdc882d846e30b731f30c9e82d5ac968b
aarce <Arce.Alexis@hotmail.com>
2021-11-08 17:57:23 -0300

    Adds mongodb and mongoose

--- Proyecto final/src/config/db.js
+++ Proyecto final/src/config/db.js
@@ -4,5 +4,14 @@ export default {
   fileSystem: {
     path: path.resolve(__dirname, "../data", "products.json"),
   },
-  mongodb: {},
+  mongodb: {
+    cnxStr:
+      "mongodb+srv://admin:09122018carp@clustercoderhouse.vludd.mongodb.net/ecommerceCoderhouse?retryWrites=true&w=majority",
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      serverSelectionTimeoutMS: 5000,
+    },
+  },
 };
